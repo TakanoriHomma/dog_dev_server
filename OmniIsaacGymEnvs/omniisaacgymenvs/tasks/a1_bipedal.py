aa6b97aebf51021285423e9355291f40f80ce4a1
TakanoriHomma <takasama_1205@yahoo.co.jp>
2023-08-02 08:56:11 +0900

    My first commit

--- OmniIsaacGymEnvs/omniisaacgymenvs/tasks/a1_bipedal.py
+++ OmniIsaacGymEnvs/omniisaacgymenvs/tasks/a1_bipedal.py
@@ -31,6 +31,7 @@ from omniisaacgymenvs.robots.articulations.a1_bipedal import A1_bipedal
 from omniisaacgymenvs.robots.articulations.views.a1_view import A1View
 from omniisaacgymenvs.tasks.utils.usd_utils import set_drive
 
+
 from omni.isaac.quadruped.utils.a1_sys_model import A1SysModel
 
 from omni.isaac.core.articulations import ArticulationView
@@ -45,6 +46,8 @@ import numpy as np
 import torch
 import math
 import matplotlib.pyplot as plt
+
+import quaternion
 
 
 class A1_bipedal_Task(RLTask):
@@ -61,21 +64,32 @@ class A1_bipedal_Task(RLTask):
         self.delta_q = 0.
 
         # normalization
+        self.lin_vel_scale = self._task_cfg["env"]["learn"]["linearVelocityScale"]
+        self.ang_vel_scale = self._task_cfg["env"]["learn"]["angularVelocityScale"]
         self.dof_pos_scale = self._task_cfg["env"]["learn"]["dofPositionScale"]
         self.dof_vel_scale = self._task_cfg["env"]["learn"]["dofVelocityScale"]
         self.action_scale = self._task_cfg["env"]["control"]["actionScale"]
 
         # reward scales
         self.rew_scales = {}
+        self.rew_scales["lin_vel_xy"] = self._task_cfg["env"]["learn"]["linearVelocityXYRewardScale"]
+        self.rew_scales["ang_vel_z"] = self._task_cfg["env"]["learn"]["angularVelocityZRewardScale"]
+        self.rew_scales["lin_vel_z"] = self._task_cfg["env"]["learn"]["linearVelocityZRewardScale"]
         self.rew_scales["joint_acc"] = self._task_cfg["env"]["learn"]["jointAccRewardScale"]
         self.rew_scales["action_rate"] = self._task_cfg["env"]["learn"]["actionRateRewardScale"]
         self.rew_scales["cosmetic"] = self._task_cfg["env"]["learn"]["cosmeticRewardScale"]
 
-
+        # command ranges
+        self.command_x_range = self._task_cfg["env"]["randomCommandVelocityRanges"]["linear_x"]
+        self.command_y_range = self._task_cfg["env"]["randomCommandVelocityRanges"]["linear_y"]
+        self.command_yaw_range = self._task_cfg["env"]["randomCommandVelocityRanges"]["yaw"]
+        
         # base init state
         pos = self._task_cfg["env"]["baseInitState"]["pos"]
         rot = self._task_cfg["env"]["baseInitState"]["rot"]
-        state = pos + rot
+        v_lin = self._task_cfg["env"]["baseInitState"]["vLinear"]
+        v_ang = self._task_cfg["env"]["baseInitState"]["vAngular"]
+        state = pos + rot + v_lin + v_ang        
 
         self.base_init_state = state
 
@@ -94,14 +108,19 @@ class A1_bipedal_Task(RLTask):
 
         self._num_envs = self._task_cfg["env"]["numEnvs"]
         self._a1_translation = torch.tensor([0.0, 0.0, 0.5])
-        self._a1_quat = torch.tensor([0.5, 0.5, -0.5, 0.5])
+        self._a1_quat = torch.tensor([0.5, 0.5, -0.5, 0.5]) # standing
+        # self._a1_quat = torch.tensor([0.0, 0.0, 0.0, 1.0]) # lying
         self._env_spacing = self._task_cfg["env"]["envSpacing"]
-        self._num_observations = 36
+        self._num_observations = 42
         self._num_actions = 12
 
         RLTask.__init__(self, name, env)
         
         self.a1_sys = A1SysModel()
+        
+        # self.potentials = 0
+        # self.potentials = to_torch([-1000./self.dt], device=self.device).repeat(self._num_envs)
+        # self.prev_potentials = self.potentials.clone()
         
         return
 
@@ -142,8 +161,8 @@ class A1_bipedal_Task(RLTask):
         joint_paths.append(f"RL_hip/RL_thigh_joint")
         joint_paths.append(f"RL_thigh/RL_calf_joint")
         for joint_path in joint_paths:
-            set_drive(f"{self._a1.prim_path}/{joint_path}", "angular", "position", 0, 45, 3, 1000)
-            
+            # set_drive(f"{self._a1.prim_path}/{joint_path}", "angular", "position", 0, 45, 3, 1000)
+            set_drive(f"{self._a1.prim_path}/{joint_path}", "angular", "position", 0, 10000, 1000, 1000)
             #PDの値設定
             # set_drive(f"{self._a1.prim_path}/{joint_path}", "angular", "position", 0, 10000, 100, 1000)
             # set_drive(f"{self._a1.prim_path}/{joint_path}", "angular", "position", 0, 400, 40, 1000)
@@ -165,6 +184,25 @@ class A1_bipedal_Task(RLTask):
         root_velocities = self._a1s.get_velocities(clone=False)
         dof_pos = self._a1s.get_joint_positions(clone=False)
         dof_vel = self._a1s.get_joint_velocities(clone=False)
+        
+        velocity = root_velocities[:, 0:3]
+        ang_velocity = root_velocities[:, 3:6]
+
+        base_lin_vel = quat_rotate_inverse(torso_rotation, velocity) * self.lin_vel_scale
+        base_ang_vel = quat_rotate_inverse(torso_rotation, ang_velocity) * self.ang_vel_scale
+        projected_gravity = quat_rotate(torso_rotation, self.gravity_vec)
+        dof_pos_scaled = (dof_pos - self.default_dof_pos) * self.dof_pos_scale
+    
+        
+        # print(projected_gravity)
+        # print(base_lin_vel)
+        print(base_ang_vel)
+
+        commands_scaled = self.commands * torch.tensor(
+            [self.lin_vel_scale, self.lin_vel_scale, self.ang_vel_scale],
+            requires_grad=False,
+            device=self.commands.device,
+        )
         
         # get foot heights
         self.FR = self._a1s._foots_heights[0][0,2:]
@@ -193,8 +231,13 @@ class A1_bipedal_Task(RLTask):
         self.foot_contact_z_RL_output = torch.where(self.foot_contact_z_RL > 0, torch.tensor(2.0, device='cuda:0'), torch.tensor(-1.0, device='cuda:0'))
         self.foot_contact_z_RR_output = torch.where(self.foot_contact_z_RR > 0, torch.tensor(2.0, device='cuda:0'), torch.tensor(-1.0, device='cuda:0'))
 
+        # self.prev_potentials = potentials.clone()
+        # self.potentials = -torch.linalg.norm(to_target, p=2, dim=-1) / self.dt
+        
         obs = torch.cat(
             (
+                base_ang_vel,
+                commands_scaled,
                 dof_pos_scaled,
                 dof_vel * self.dof_vel_scale,
                 self.actions,
@@ -224,22 +267,21 @@ class A1_bipedal_Task(RLTask):
         
         
         # 各関節のアクションを0
-        # self.actions[:,0] = torch.tensor(0)
-        # self.actions[:,2] = torch.tensor(0)
-        # self.actions[:,3] = torch.tensor(0)
-        # self.actions[:,4] = torch.tensor(0)
-        # self.actions[:,6] = torch.tensor(0)
-        # self.actions[:,7] = torch.tensor(0)
-        # self.actions[:,8] = torch.tensor(0)
-        # self.actions[:,10] = torch.tensor(0)
-        # self.actions[:,11] = torch.tensor(0)
+        # self.actions[:,0] = torch.tensor(0) # FL_hip
+        # self.actions[:,1] = torch.tensor(0) # FR_hip
+        # self.actions[:,2] = torch.tensor(0) # RL_hip
+        # self.actions[:,3] = torch.tensor(0) # RR_hip
+        # self.actions[:,4] = torch.tensor(-np.pi/12) # FL_thigh
+        # self.actions[:,5] = torch.tensor(-np.pi/12) # FR_thigh
+        # self.actions[:,6] = torch.tensor(0) # RL_thigh
+        # self.actions[:,7] = torch.tensor(0) # RR_thigh
+        # self.actions[:,8] = torch.tensor(np.pi/12) # FL_calf
+        # self.actions[:,9] = torch.tensor(np.pi/12) # FR_calf
+        # self.actions[:,10] = torch.tensor(0) # RL_calf
+        # self.actions[:,11] = torch.tensor(0) # RR_calf
         
-        # 右前足関節のアクションを0
-        # self.actions[:,1] = torch.tensor(0)
-        # self.actions[:,5] = torch.tensor(0)
-        # self.actions[:,9] = torch.tensor(0)
         
-        #40step待つ(着地後安定化させるため)
+        # 40step待つ(着地後安定化させるため)
         # self.actions = torch.where(self.progress_buf.repeat_interleave(self._num_actions) > 40, self.actions.flatten(), 0)
         # self.actions = self.actions.view(self._num_envs, -1)
         
@@ -282,7 +324,11 @@ class A1_bipedal_Task(RLTask):
         dof_limits = self._a1s.get_dof_limits()
         self.a1_dof_lower_limits = dof_limits[0, :, 0].to(device=self._device)
         self.a1_dof_upper_limits = dof_limits[0, :, 1].to(device=self._device)
-
+        
+        self.commands = torch.zeros(self._num_envs, 3, dtype=torch.float, device=self._device, requires_grad=False)
+        self.commands_y = self.commands.view(self._num_envs, 3)[..., 1]
+        self.commands_x = self.commands.view(self._num_envs, 3)[..., 0]
+        self.commands_yaw = self.commands.view(self._num_envs, 3)[..., 2]
 
         # initialize some data used later on
         self.extras = {}
@@ -313,6 +359,11 @@ class A1_bipedal_Task(RLTask):
         dof_pos = self._a1s.get_joint_positions(clone=False)
         dof_vel = self._a1s.get_joint_velocities(clone=False)
 
+        velocity = root_velocities[:, 0:3]
+        ang_velocity = root_velocities[:, 3:6]
+        
+        base_lin_vel = quat_rotate_inverse(torso_rotation, velocity)
+        base_ang_vel = quat_rotate_inverse(torso_rotation, ang_velocity)
 
         # velocity tracking reward
         
@@ -330,11 +381,70 @@ class A1_bipedal_Task(RLTask):
         
         rew_center = x_range & y_range & z_range
 
-        rew_foot_contact = self.foot_contact_z_FR_output + self.foot_contact_z_FL_output + self.foot_contact_z_RR_output + self.foot_contact_z_RL_output
+        # 前脚を離したい
+        # self.foot_contact_z_FR_outputは，ついてたら2，ついてなかったら-1を格納
+        # print(self.foot_contact_z_FR_output)
+        rew_foot_contact = -(self.foot_contact_z_FR_output \
+                            + self.foot_contact_z_FL_output)
         
-        total_reward = rew_torsodistance*3 + rew_center*10 + rew_foot_contact
+        # 後脚を接地させたい
+        # rew_foot_contact += (self.foot_contact_z_RR_output \
+        #                     + self.foot_contact_z_RL_output) * 0.1
+                            
+        # print(rew_foot_contact)
+
+        # total_reward = rew_torsodistance*3 + rew_center*10 + rew_foot_contact
+        # fr = self.foot_contact_z_FR_output.to('cpu').detach().numpy().copy()
+        # fl = self.foot_contact_z_FL_output.to('cpu').detach().numpy().copy()
+        # rr = self.foot_contact_z_RR_output.to('cpu').detach().numpy().copy()
+        # rl = self.foot_contact_z_RL_output.to('cpu').detach().numpy().copy()
+
+        # やりたいこと
+        # 胴体のz座標に応じて報酬
+        # 0.45に近いほど報酬
+        # print(fr[0], fl[0], rr[0], rl[0])
+        # print(0.45 - self.torso_z)
+        # rew_z = 1.0 - torch.abs(0.45 - self.torso_z)
+        # print(rew_z)
+        rew_alive = torch.ones_like(self.torso_x) * 2.0
+        # print(rew_alive)
+        z_low = 0.45
+        z_up = 0.5
+        isZgood = (self.torso_z.mean() > z_low) and (self.torso_z.mean() < z_up)
+        # rew_z = isZgood * 10
+        rew_z = self.torso_z*0 + isZgood*10
+        # print(rew_z)
         
+        # print(rew_z)
         
+        # print(torso_rotation.to('cpu').detach().numpy().copy())
+        # print(quaternion.as_euler_angles(torso_rotation.to('cpu').detach().numpy().copy()))
+        a = quaternion.as_quat_array(torso_rotation.to('cpu').detach().numpy().copy())
+        b = quaternion.as_euler_angles(a)
+        c = np.rad2deg(b)
+        d = c.T
+        e_1 = d[:1][0]
+        e_2 = d[1:2][0]
+        f_1 = torch.from_numpy(e_1.astype(np.float32)).clone().to(self.device)
+        f_2 = torch.from_numpy(e_2.astype(np.float32)).clone().to(self.device)
+        # print(f_1+90)
+        # print(f_2) 
+        roll_element = -(torch.abs(f_1 + 90)**2)
+        pitch_element = -(torch.abs(f_2 - 90)**2)
+        rew_tilt = (- torch.abs(f_1 + 90) - torch.abs(f_2 - 90))*0.001
+        # print(rew_tilt)
+        # print(rew_z)
+        
+        # total_reward = rew_z\
+        #                 + rew_tilt
+        
+        # print(root_velocities)
+        # rew_vel = .0
+    
+        # total_reward = rew_z
+        total_reward = rew_tilt
+        # total_reward = rew_z + rew_foot_contact        
+        # total_reward = rew_tilt + rew_foot_contact + rew_z
 
         # print(total_reward)
         total_reward = torch.clip(total_reward, 0.0, None)
@@ -342,7 +452,7 @@ class A1_bipedal_Task(RLTask):
         self.last_actions[:] = self.actions[:]
         self.last_dof_vel[:] = dof_vel[:]
 
-        self.fallen_over = self._a1s.is_base_below_threshold(threshold=0.2, ground_heights=0.0)
+        self.fallen_over = self._a1s.is_base_below_threshold(threshold=0.4, ground_heights=0.0)
         total_reward[torch.nonzero(self.fallen_over)] = -1
         self.rew_buf[:] = total_reward.detach()
         
